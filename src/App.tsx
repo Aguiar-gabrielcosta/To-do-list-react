d5736380fdf48c453b17bc88688570e0ca3a5752
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-04-30 18:00:46 -0300

    Adiciona a classe Tarefas e o Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { Provider } from 'react-redux'
 import Barralateral from './containers/BarraLateral'
 import ListaDetarefas from './containers/ListaDeTarefas'
 import EstiloGlobal, { Container } from './styles'
+import store from './store'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
       <Container>
         <Barralateral />
         <ListaDetarefas />
       </Container>
-    </>
+    </Provider>
   )
 }
 
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,29 +1,11 @@
+import { useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
-import * as enums from '../../utils/enums/Tarefa'
-
-const tarefas = [
-  {
-    titulo: 'Estudar TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  },
-  {
-    titulo: 'Pagar a conta de Internet',
-    descricao: 'Baixar fatura do Gmail',
-    prioridade: enums.Prioridade.URGENTE,
-    status: enums.Status.CONCLUIDA
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino B',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  }
-]
 
 const ListaDetarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+
   return (
     <Container>
       <p>
