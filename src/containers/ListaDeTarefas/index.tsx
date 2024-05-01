5268395ec01db4951d66b9583b3214f9eef67913
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-05-01 13:40:19 -0300

    Adiciona a funcionalidade de remoção de tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -4,7 +4,7 @@ import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
 const ListaDetarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state)
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
 
   return (
     <Container>
@@ -12,13 +12,14 @@ const ListaDetarefas = () => {
         2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
       </p>
       <ul>
-        {tarefas.map((t) => (
-          <li key={t.titulo}>
+        {itens.map((t) => (
+          <li key={t.id}>
             <Tarefa
               titulo={t.titulo}
               descricao={t.descricao}
               status={t.status}
               prioridade={t.prioridade}
+              id={t.id}
             />
           </li>
         ))}
--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -2,34 +2,42 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefa'
 
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: '',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 2,
+      descricao: 'Rever  aula 2',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 3,
+      descricao: 'praticar useEffect',
+      prioridade: enums.Prioridade.URGENTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar React'
+    }
+  ]
+}
+
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 2',
-      2
-    ),
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'praticar o useEffect',
-      3
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id != action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id != action.payload)
     }
   }
 })
